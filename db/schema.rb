c5d4f0b798df543b60d45c4da032bc93f2593425
JCallahan16 <vagrant@kellogg-dev-box>
2014-04-29 15:58:06 +0000

    First commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140429153200) do
+ActiveRecord::Schema.define(version: 20140429155343) do
 
   create_table "courses", force: true do |t|
     t.string  "title"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+eh = Course.create("title" => "European History", "max_enrollment" => "12", "teacher_id" => "1", "student_id" => "4")
+mb = Course.create("title" => "Microbiology", "max_enrollment" => "10", "teacher_id" => "1", "student_id" => "4")
+df = Course.create("title" => "Dinosaurs & Other Failures", "max_enrollment" => "20", "teacher_id" => "2", "student_id" => "9")
+rl = Course.create("title" => "Russian Literature", "max_enrollment" => "15", "teacher_id" => "3", "student_id" => "7")
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
