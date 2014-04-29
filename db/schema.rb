11ea2dc019e0910084fc04c735a60bac03635774
vagrant <vagrant@kellogg-dev-box>
2014-04-29 15:32:22 +0000

    first push

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140429114254) do
+ActiveRecord::Schema.define(version: 20140429153200) do
 
   create_table "courses", force: true do |t|
     t.string  "title"
@@ -21,8 +21,7 @@ ActiveRecord::Schema.define(version: 20140429114254) do
   end
 
   create_table "students", force: true do |t|
-    t.string  "name"
-    t.integer "course_id"
+    t.string "name"
   end
 
   create_table "teachers", force: true do |t|
